570cef4106d58bd07fe90102e6002535ccf58051
benbugraer <bugraerdev@gmail.com>
2024-05-08 22:07:14 +0300

    Back VSCode

--- app/gear/components/GearItems.jsx
+++ app/gear/components/GearItems.jsx
@@ -1,99 +1,99 @@
-import {LuLaptop2} from "react-icons/lu";
+import { LuLaptop2 } from "react-icons/lu";
 
-import {BsFillMouseFill} from "react-icons/bs";
-import {TbDeviceAirpods} from "react-icons/tb";
-import {FaKeyboard} from "react-icons/fa6";
-import {FaFigma} from "react-icons/fa";
+import { BsFillMouseFill } from "react-icons/bs";
+import { TbDeviceAirpods } from "react-icons/tb";
+import { FaKeyboard } from "react-icons/fa6";
+import { FaFigma } from "react-icons/fa";
 import {
-    SiWindows,
-    SiNotion,
-    SiMiro,
-    SiArc,
-    SiOpenai,
-    SiFramer,
-    SiGooglechrome,
-    SiWebstorm,
+  SiWindows,
+  SiNotion,
+  SiMiro,
+  SiArc,
+  SiOpenai,
+  SiFramer,
+  SiGooglechrome,
+  SiVisualstudiocode,
 } from "react-icons/si";
 
-const GearItem = ({icon, title}) => (
-    <li className="flex items-center gap-6">
-        {icon}
-        <h1>{title}</h1>
-    </li>
+const GearItem = ({ icon, title }) => (
+  <li className="flex items-center gap-6">
+    {icon}
+    <h1>{title}</h1>
+  </li>
 );
 
-const GearItemsSection = ({title, items}) => (
-    <section className="flex flex-col gap-8 animate-in" style={{"--index": 3}}>
-        <h2 className="text-secondary">{title}</h2>
-        <ul className="grid md:grid-cols-2 gap-x-4 gap-y-4 mb-4 lg:-mb-5   animated-list">
-            {items.map((item, index) => (
-                <GearItem key={index} {...item} />
-            ))}
-        </ul>
-    </section>
+const GearItemsSection = ({ title, items }) => (
+  <section className="flex flex-col gap-8 animate-in" style={{ "--index": 3 }}>
+    <h2 className="text-secondary">{title}</h2>
+    <ul className="grid md:grid-cols-2 gap-x-4 gap-y-4 mb-4 lg:-mb-5   animated-list">
+      {items.map((item, index) => (
+        <GearItem key={index} {...item} />
+      ))}
+    </ul>
+  </section>
 );
 
 const GearItems = () => {
-    const techGear = [
-        {icon: <LuLaptop2 className="w-7 h-7"/>, title: "Huawei Matebook 16"},
-        {
-            icon: <BsFillMouseFill className="w-7 h-7"/>,
-            title: "Logitech Pebble 2 M350S",
-        },
-        {
-            icon: <FaKeyboard className="w-7 h-7"/>,
-            title: "Logitech Pebble 2 K380S",
-        },
-        {
-            icon: <TbDeviceAirpods className="w-7 h-7"/>,
-            title: "Huawei Freebuds SE 2",
-        },
-    ];
+  const techGear = [
+    { icon: <LuLaptop2 className="w-7 h-7" />, title: "Huawei Matebook 16" },
+    {
+      icon: <BsFillMouseFill className="w-7 h-7" />,
+      title: "Logitech Pebble 2 M350S",
+    },
+    {
+      icon: <FaKeyboard className="w-7 h-7" />,
+      title: "Logitech Pebble 2 K380S",
+    },
+    {
+      icon: <TbDeviceAirpods className="w-7 h-7" />,
+      title: "Huawei Freebuds SE 2",
+    },
+  ];
 
-    const codingTools = [
-        {
-            icon: <SiWindows className="w-7 h-7"/>,
-            title: "Windows 11 Home Edition",
-        },
-        {
-            icon: <SiWebstorm className="w-7 h-7"/>,
-            title: "Webstorm | Light Theme",
-        },
-        {
-            icon: <SiArc className="w-7 h-7"/>,
-            title: "Arc Browser",
-        },
-        {
-            icon: <SiGooglechrome className="w-7 h-7"/>,
-            title: "Google Chrome",
-        },
-        {
-            icon: <SiOpenai className="w-7 h-7"/>,
-            title: "Chat GPT",
-        },
-    ];
+  const codingTools = [
+    {
+      icon: <SiWindows className="w-7 h-7" />,
+      title: "Windows 11 Home Edition",
+    },
+    {
+      icon: <SiVisualstudiocode className="w-7 h-7" />,
+      title: "Visual Studio Code",
+    },
+    {
+      icon: <SiArc className="w-7 h-7" />,
+      title: "Arc Browser",
+    },
+    {
+      icon: <SiGooglechrome className="w-7 h-7" />,
+      title: "Google Chrome",
+    },
+    {
+      icon: <SiOpenai className="w-7 h-7" />,
+      title: "Chat GPT",
+    },
+  ];
 
-    const designTools = [
-        {
-            icon: <FaFigma className="w-7 h-7"/>,
-            title: "Figma",
-        },
-        {icon: <SiFramer className="w-7 h-7"/>, title: "Framer"},
-    ];
+  const designTools = [
+    {
+      icon: <FaFigma className="w-7 h-7" />,
+      title: "Figma",
+    },
+    { icon: <SiFramer className="w-7 h-7" />, title: "Framer" },
+  ];
 
-    const productivityApps = [
-        {icon: <SiNotion className="w-7 h-7"/>, title: "Notion"},
-        {icon: <SiMiro className="w-7 h-7"/>, title: "Miro"},
-    ];
+  const productivityApps = [
+    { icon: <SiNotion className="w-7 h-7" />, title: "Notion" },
+    { icon: <SiMiro className="w-7 h-7" />, title: "Miro" },
+  ];
 
-    return (
-        <>
-            <GearItemsSection title="Tech Gear" items={techGear}/>
-            <GearItemsSection title="Coding Tools" items={codingTools}/>
-            <GearItemsSection title="UI/UX Design" items={designTools}/>
-            <GearItemsSection title="Productivity Apps" items={productivityApps}/>
-        </>
-    );
+  return (
+    <>
+      <GearItemsSection title="Tech Gear" items={techGear} />
+      <GearItemsSection title="Coding Tools" items={codingTools} />
+      <GearItemsSection title="UI/UX Design" items={designTools} />
+      <GearItemsSection title="Productivity Apps" items={productivityApps} />
+    </>
+  );
 };
 
 export default GearItems;
--- app/gear/page.jsx
+++ app/gear/page.jsx
@@ -1,30 +1,30 @@
 import GearItems from "./components/GearItems";
 
 export const metadata = {
-  title: "Bugra Er | Gear",
-  description: "All the tools and applications I use",
+    title: "Bugra Er | Gear",
+    description: "All the tools and applications I use",
 };
 
 export default function Gear() {
-  return (
-    <>
-      <div className="flex flex-col  gap-12 md:gap-24">
-        <div className="flex flex-col gap-12 animate-in">
-          <div>
-            <h1 className="animate-in text-3xl font-bold tracking-tight">
-              Gear & Tools & Apps
-            </h1>
-            <p className="animate-in text-secondary" style={{ "--index": 1 }}>
-              All tools i can use
-            </p>
-          </div>
-          <p className="max-w-lg animate-in" style={{ "--index": 2 }}>
-            All these tools and applications make my life easier and i enjoy
-            using them.
-          </p>
-          <GearItems />
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="flex flex-col  gap-12 md:gap-24">
+                <div className="flex flex-col gap-12 animate-in">
+                    <div>
+                        <h1 className="animate-in text-3xl font-bold tracking-tight">
+                            Gear & Tools & Apps
+                        </h1>
+                        <p className="animate-in text-secondary" style={{"--index": 1}}>
+                            All tools i can use
+                        </p>
+                    </div>
+                    <p className="max-w-lg animate-in" style={{"--index": 2}}>
+                        All these tools and applications make my life easier and i enjoy
+                        using them.
+                    </p>
+                    <GearItems/>
+                </div>
+            </div>
+        </>
+    );
 }
